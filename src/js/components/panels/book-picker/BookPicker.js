29537e0ad3ecd10d5905d34d11755e0c07477581
Ethan Smith <ethan@onebytegone.com>
2018-01-07 16:08:16 -0500

    Colorize book picker

--- src/js/components/panels/book-picker/BookPicker.js
+++ src/js/components/panels/book-picker/BookPicker.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var template = require('./book-picker.html');
+var _ = require('underscore'),
+    template = require('./book-picker.html');
 
 module.exports = {
 
@@ -11,7 +12,37 @@ module.exports = {
       books: function() {
          var edition = this.$store.state.edition;
 
-         return edition ? edition.books : undefined;
+         if (edition) {
+            return _.map(_.extend({}, edition.books), function(book, index) {
+               if (index < 5) {
+                  book.color = 'colorThree';
+               } else if (index < 18) {
+                  book.color = 'colorOne';
+               } else if (index < 23) {
+                  book.color = 'colorTwo';
+               } else if (index < 44) {
+                  book.color = 'colorThree';
+               } else if (index < 45) {
+                  book.color = 'colorOne';
+               } else if (index < 66) {
+                  book.color = 'colorTwo';
+               } else {
+                  book.color = 'colorThree';
+               }
+
+               return book;
+            });
+         }
+
+         return undefined;
+      },
+
+      hebrewBooks: function() {
+         return this.books ? this.books.slice(0, 39) : undefined;
+      },
+
+      greekBooks: function() {
+         return this.books ? this.books.slice(39) : undefined;
       },
 
    },
